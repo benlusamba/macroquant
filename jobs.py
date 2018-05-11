8d8fa8ab64ff68d662189db43f01baf54a773107
Ben Lusamba <benlusamba@gmail.com>
2018-05-11 15:05:40 -0400

    Typo

--- jobs.py
+++ jobs.py
@@ -1,6 +1,6 @@
 # Retrieving U.S. unenmployment data, using the BLS API (automated retrival)
 
-import requests                                           # Import the relevant modules
+import requests                                           
 import json
 import prettytable
 headers = {'Content-type': 'application/json'}
@@ -8,16 +8,12 @@ data = json.dumps({"seriesid": ['LNS14000000'],"startyear":"2009", "endyear":"20
 p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
 json_data = json.loads(p.text)
 for series in json_data['Results']['series']:
-    x=prettytable.PrettyTable(["series id","year","period","value"]) #add "footnotes"
+    x=prettytable.PrettyTable(["series id","year","period","value"])
     seriesId = series['seriesID']
     for item in series['data']:
         year = item['year']
         period = item['period']
         value = item['value']
-        #footnotes=""
-        #for footnote in item['footnotes']:
-        #    if footnote:
-                #footnotes = footnotes + footnote['text'] + ','
 
         x.add_row([seriesId,year,period,value])
 
